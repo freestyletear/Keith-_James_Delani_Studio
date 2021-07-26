626255598b16f5315e4cac8c9657c27c31c3ad7b
freestyletear <freestyletear@gmail.com>
2021-07-26 05:19:33 +0300

    modify contact section

--- js/scripts.js
+++ js/scripts.js
@@ -131,6 +131,50 @@ $(document).ready(function () {
 });
 
 
+
+// Form validation and acknowledment
+let collectInput = () => {
+    if ($(`#userName`).val() == ``) {
+        alert(`Please fill in your name`);
+    } else if ($(`#userMail`).val() == ``) {
+        alert(`Please provide your E-mail`)
+    }
+     else if ($(`#userMessage`).val() == ``) {
+        alert(`We'd love to hear your feedback. Please fill it in`)
+    }
+     else {
+    userName = $(`#userName`).val();
+    alert(userName + ` we have recieved your message. Thank you for reaching out to us`)
+    }
+}
+
+
+
+//
+// $("#contact-form form").submit(function (e) {
+//         e.preventDefault();
+
+//         var name = $("#name").val();
+//         var email = $("#email").val();
+//         var message = $("#message").val();
+//         // var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+
+//         // Check for empty fields
+//         if (name == '' || email == '' || message == '') {
+//             $("#name-error").html("<p class='text-danger'>Please fill in you name.</p>");
+//             $("#email-error").html("<p class='text-danger'>Please fill in your valid email.</p>");
+//             $("#message-error").html("<p class='text-danger'>Please fill in the field.</p>");
+//         } else if (email == '' || !emailReg.test(email)) {
+//             $("#email-error").html("<p class='text-danger'>Please enter a valid email address.</p>");
+//         } else {
+//             $("#name-error").html("");
+//             $("#email-error").html("");
+//             $("#message-error").html("");
+//             $(".alert-success").text(`${name} we have received your message. Thank you for reaching out to us.`)
+//             $(".alert-success").show();
+//         }
+
+
 // $(document).ready(function () {
 //   $("#button").click(function () {
 //     $(".bg-modal").show();
@@ -150,15 +194,15 @@ $(document).ready(function () {
 //     $(".bg-modal").show();
 
 
-document.getElementById('button').addEventListener('click',
-function(){
-  document.querySelector('.bg-modal').style.display = 'flex';
-});
+// document.getElementById('button').addEventListener('click',
+// function(){
+//   document.querySelector('.bg-modal').style.display = 'flex';
+// });
 
-document.querySelector('.close').addEventListener('click', 
-function() {
-  document.querySelector('.bg-modal').style.display = 'none';
-});
+// document.querySelector('.close').addEventListener('click', 
+// function() {
+//   document.querySelector('.bg-modal').style.display = 'none';
+// });
 
 // var myModal = document.getElementById(".bg-modal");
 // var myInput = document.getElementById("button");
